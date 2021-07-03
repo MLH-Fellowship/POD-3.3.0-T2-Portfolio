656ab984cd3dcb5ba7900d3ce74119b5cbc576af
Cloud User <centos@ip-172-31-56-186.ec2.internal>
2021-07-03 02:19:50 +0000

    week 3 changes

---
+++ endpointTest.sh
@@ -0,0 +1,29 @@
+#! /bin/sh
+#Test all endpoints of domain and return success/failure
+
+function test {
+   domain="https://patrick-portfolio.duckdns.org"
+   declare -a endpoints=("" "health" "patrick" "athena" "juancarlos" "register" "login")
+      
+   #Generate random user and password
+   username=$(date | md5sum)
+   password=$(date | md5sum)
+
+   for endpoint in "${endpoints[@]}"
+   do
+      url="${domain}/${endpoint}" 
+      
+      if [[ ${endpoint} == "register" ]]
+      then 
+         curl -X POST -d "username=${username}&password=${password}" "${url}" 
+      elif [[ ${endpoint} == "login" ]]
+      then 
+         curl -X POST -d "username=${username}&password=${password}" "${url}" 
+      else
+         status=$(curl -s -o /dev/null -w "%{http_code}\n" "${url}") 
+         echo "Status Code [${status}] for ${url}"
+      fi
+   done
+}
+
+test
