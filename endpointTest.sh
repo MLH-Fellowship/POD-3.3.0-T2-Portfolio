d6353f974dcef881d1c4b30f206308830a6cd6b1
Cloud User <centos@ip-172-31-56-186.ec2.internal>
2021-07-05 20:59:39 +0000

    deleted example.env, modified endpoint to match register, and modified register endpoint

--- endpointTest.sh
+++ endpointTest.sh
@@ -4,22 +4,24 @@
 function test {
    httpDomain="http://patrick-portfolio.duckdns.org"
    httpsDomain="https://patrick-portfolio.duckdns.org"
-   declare -a endpoints=("" "health" "patrick" "athena" "juancarlos" "register" "login")
+   declare -a endpoints=("" "health" "patrick" "athena" "juancarlos" "register")
 
    #Generate random user and password
    username=$(date | md5sum)
    password=$(date | md5sum)
-
+   echo "User: $username \n Password: $password"
    for endpoint in "${endpoints[@]}"
    do
       url="${httpDomain}/${endpoint}"
 
       if [[ ${endpoint} == "register" ]]
       then
-         curl -X POST -d "username=${username}&password=${password}" "${url}"
+        # curl -X POST -d "username=${username}&password=${password}" "${url}"
+         curl -X POST -d "username=patrick&password=12345" "${url}"
       elif [[ ${endpoint} == "login" ]]
       then
-         curl -X POST -d "username=${username}&password=${password}" "${url}"
+         #curl -X POST -d "username=${username}&password=${password}" "${url}"
+         curl -X POST -d "username=patrick&password=12345" "${url}"
       else
          status=$(curl -s -o /dev/null -w "%{http_code}\n" "${url}")
          echo "Status Code [${status}] for ${url}"
